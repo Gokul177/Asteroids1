49aa0869166beca6b7cca06af142ef96978eb913
Gokul177 <49999386+Gokul177@users.noreply.github.com>
2019-05-14 15:01:55 -0700

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -1,9 +1,37 @@
-
+import java.awt.Graphics;
 /**
  * 
  * @author gokuldeep
  *
  */
 public class Projectile {
+  
+  private static final String PATH_PREFIX = "/";
+	private int row ;
+	private int col;
+	public Image Img;
+	public double SCALE_FACTOR = 0.04;
+	public Projectile (int r, int c){
+		row = r;
+		col = c;
+		Img = getImage();
+	}
+	public Image getImage() {
+		Image Img = null;
+		String getter = PATH_PREFIX+ "ball.png";
+		try{
+			Img = ImageIO.read(getClass().getResource(getter));
+		}catch (IOException e) {
+			
+		}
+		return Img;
+		
+	}
+	public void draw (Graphics g) {
+		g.drawImage(Img, 100, 100, null);
+	}
+	
+	
+
 
 }
