71f11291a4e767c0492a8f4f85ce858056988a15
KeoniFederico <49999394+KeoniFederico@users.noreply.github.com>
2019-05-29 15:16:07 -0700

    Update AsteroidGame.java

--- AsteroidGame.java
+++ AsteroidGame.java
@@ -1,72 +1,99 @@
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Toolkit;
 import java.util.ArrayList;
-import java.util.List;
 
-public class AsteroidGame{
-	private ArrayList<Gameobject> GameObjects;
-	private Gameobject BigAsteroid;
-	private Gameobject SmallAsteroid;
-	public int Score;
-	public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	public static final int WIDTH = (int) (screenSize.getWidth()*3/4),HEIGHT=(int) (screenSize.getHeight()*3/4);
-
-	
-	public AsteroidGame(){
-		
-		ArrayList<Gameobject> GameObjects = new ArrayList<>();
-		this.Score++;
-			
-	}
-	private void remove(){
-		for(int i=GameObjects.size(); i < 0; i--){
-			int a = GameObjects.get(i).getHealth();
-			if(a == 0){
-				GameObjects.remove(i);
-				Score++;
-			}	
-		}	
+public class AsteroidGame {
+	ProjectileShip alive;
+	ArrayList<Projectile> bullet = new ArrayList<Projectile>();;
+	ArrayList<Gameobject> Asteroid = new ArrayList<Gameobject>();
+	String direction;
+	int numBullets = 0;
+	int index = 0;
+	public AsteroidGame() {
+		 alive = new ProjectileShip(500, 300);
 	}
 
-	private void addAsteroid(){
-		int r = (int)(Math.random()*2);
-		for(int i = 0; i<=10; i++){
-			if(r<1){
-				
-				GameObjects.add(BigAsteroid);
-			}
-			else{
-				GameObjects.add(SmallAsteroid);
-			}
-		}
-		
-	}
-	
-	public void moveAcrossScreen(Graphics j) { 
-		for(int x = 100; x<WIDTH; x+=100){ 
-			for(int y = 50; y<HEIGHT; y+=100){ 
-				j.drawOval(x, y, 100, 100); 
-				g.dispose(); 
-				} 
-			} 
-		}
 	
 	public void keyHit(String s) {
-		// TODO Auto-generated method stub
-		
+		direction = s;
+		alive.move(s);
+	
 	}
 
-	public void movewith() {
-		// TODO Auto-generated method stub
-		
+	public void shoot() {
+			bullet.add(new Projectile(alive,alive.rotation1));
+			numBullets+=1;
+			System.out.println("number of bullet " + numBullets);
 	}
 
 	public boolean collide() {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < Gameobjects.size(); i ++){
+			if(Gameobjects.get(i).overlaps(ProjectileShip)){
+				return true;
+				//end game
+				}	
+			}	
+		}
+		for(int i = 0; i < Gameobjects.size(); i ++){
+			for(int o; o < Projectile.size(); o++){
+				if((Gameobjects.get(i)).overlaps(Projectile.get(o))){
+					Projectile.remove(o);
+					if(//Gameobject.get(i).isBig){
+						int x = Gameobject.get(i)//.getXpos
+						int y = Gameobject.get(i)//.getYpos
+						double z = (int)(Math.random()*2);
+						int ax = x + Math.cos(z)*50;
+						int ay = y + Math.sin(z)*50;
+						int bx = x + Math.cos(z)*50;
+						int by = y + Math.sin(z)*50;
+						//new asteriod a at (ax,ay) and radius of 50
+						//new asteriod b at (bx,by) and radius of fifty
+						Gameobject.add(a);
+						Gameobject.add(b);
+						
+					else{
+						Gameobject.remove(i)
+					}	
+				}		
+			}
+		}
 		return false;
 	}
+	public void createAsteroid() {
+		Gameobject ast = new Gameobject(alive);
+			Asteroid.add(ast);
+			
+	}
 
+	public void draw(Graphics g) {
+		alive.draw(g);
+		createAsteroid();
+		ArrayList<Gameobject> movingA = new ArrayList<>();
+			for(int i = 1; i< Asteroid.size()-1; i++) {
+			if( Asteroid.get(i).overlaps(Asteroid.get(i-1))) {
+				movingA.add(Asteroid.get(i));
+			}else {
+				Asteroid.get(i).draw(g);
+			}
+		}	
+			for (Gameobject p : movingA) {
+				System.out.println("removedAsteroid");
+				Asteroid.remove(p);
+			}			
+		ArrayList<Projectile> bulletToRemove = new ArrayList<>();
+		for (Projectile p:bullet) {
+			if (p.pos <= -1000) {  // upper bound ?
+				bulletToRemove.add(p);
+			} else {
+				p.draw(g);
+			}
+		}
+		for (Projectile p : bulletToRemove) {
+			//System.out.println("removed bullet");
+			bullet.remove(p);
+		}			
+				
+	}
+	
+		
 
 }
