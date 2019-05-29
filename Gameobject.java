0346d231e1921d74c8418459ff615575f9114e33
KeoniFederico <49999394+KeoniFederico@users.noreply.github.com>
2019-05-29 15:19:41 -0700

    Update Gameobject.java

--- Gameobject.java
+++ Gameobject.java
@@ -1,94 +1,65 @@
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Rectangle;
-import java.awt.Shape;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.PathIterator;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javafx.scene.shape.Circle;
+import javax.swing.Timer;
 
-import javax.imageio.ImageIO;
-
-
-public class Gameobject implements Shape{
-	public int health;
-	public int x ,y;
-	public int width;
-	public int height;
-	private Shape shape;
-	boolean isCollide;
+public class Gameobject implements ActionListener{
+	private int X;
+	private int Y; 
+	private int health;
+	private Rectangle rect;
+	int pos;
 	
-	public Gameobject(int center, int radius) {
-		Circle circ1 = new Circle();
-		circ1.setRadius((double)radius);
+	public Gameobject(ProjectileShip s) {
+	  pos = (int)(800 * Math.random());
+	  this.X = (int)(400 * Math.random());
+	  rect = new Rectangle(this.X, pos);
+		t.start();
+	}
+	 Timer t= new Timer(20, (ActionListener) this);
+	int vel = 1;
+		
+	
+	public void draw(Graphics g) {
+	if( this.X <=800)
+		g.drawOval(this.X, pos, 100, 100);
+	
+	}
+/*	public int area() {
+		
+	}*/
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (rect.getHeight() > 400)
+			this.X = this.X + vel;
+		if (rect.getWidth() < 400)
+			this.pos = this.pos - vel;
+		if (rect.getWidth() > 400)
+			this.pos = this.pos + vel;
+		if (rect.getHeight() < 400)
+			this.X = this.X + vel;
 		
 	}
+	
+	public Rectangle rect() {
 
-public void draw(Graphics g) {
-		g.drawOval(x,y,width,height);
+		return rect;
 	}
-
-	@Override
-	public boolean contains(Point2D arg0) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean overlaps( Gameobject g) {
+		return this.rect().intersects(g.rect());
 	}
-	@Override
-	public boolean contains(Rectangle2D arg0) {
-		// TODO Auto-generated method stub
-		return false;
+	public int getHealth() {
+		return health;
 	}
-	@Override
-	public boolean contains(double arg0, double arg1) {
-		// TODO Auto-generated method stub
-		return false;
+	public int setHealth(int n){
+		 health = n;
 	}
-	@Override
-	public boolean contains(double arg0, double arg1, double arg2, double arg3) {
-		// TODO Auto-generated method stub
-		return false;
+	public int getXpos(){
+		return x;
 	}
-	@Override
-	public Rectangle getBounds() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public Rectangle2D getBounds2D() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public PathIterator getPathIterator(AffineTransform arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public PathIterator getPathIterator(AffineTransform arg0, double arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public boolean intersects(Rectangle2D arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	@Override
-	public boolean intersects(double arg0, double arg1, double arg2, double arg3) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	public int getHealth(){
-		return this.health;
-	}
-	public void remove(){
-		if(this.getHealth()<=0){
-		}
-
+	public int getYpos(){
+		return y;
 	}
 }
